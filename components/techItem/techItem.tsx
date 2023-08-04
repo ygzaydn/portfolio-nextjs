14510de1c9ac4e50001017c52de3d848d35232fd
ygzaydn <ygzaydns@gmail.com>
2023-08-04 14:49:42 +0300

    styling

--- components/techItem/techItem.tsx
+++ components/techItem/techItem.tsx
@@ -3,34 +3,32 @@ import Image from "next/image";
 import { useState, useEffect } from "react";
 
 interface TechItemProps {
-  logo: string;
-  name: string;
+    logo: string;
+    name: string;
 }
 
 const TechItem: React.FC<TechItemProps> = ({ logo, name }) => {
-  const [url, setUrl] = useState<string>("");
+    const [url, setUrl] = useState<string>("");
 
-  useEffect(() => {
-    setUrl(require(`../../public/logo/${logo}.svg`).default?.src);
-  }, []);
+    useEffect(() => {
+        setUrl(require(`../../public/logo/${logo}.svg`).default?.src);
+    }, []);
 
-  return (
-    <div className="group mx-10 relative flex justify-center [&>img]:hover:brightness-0 [&>h6]:hover:flex">
-      {url !== "" && (
-        <img
-          height={75}
-          width={75}
-          src={url}
-          alt={logo}
-          className="w-16 h-16 transition"
-        />
-      )}
+    return (
+        <div className="group mx-10 relative sm:mx-6 flex justify-center [&>img]:hover:brightness-0 [&>h6]:hover:flex">
+            {url !== "" && (
+                <img
+                    src={url}
+                    alt={logo}
+                    className="w-16 h-16 sm:w-12 sm:h-12 transition"
+                />
+            )}
 
-      <h6 className="my-auto text-blue-400 hidden absolute text-center h-16 items-center -rotate-6 text-xl cursor-default">
-        {name}
-      </h6>
-    </div>
-  );
+            <h6 className="my-auto text-blue-400 hidden absolute text-center h-16 items-center -rotate-6 text-xl cursor-default">
+                {name}
+            </h6>
+        </div>
+    );
 };
 
 export default TechItem;
--- components/techStack/techStack.tsx
+++ components/techStack/techStack.tsx
@@ -3,16 +3,16 @@ import { technologies } from "../../constants/technologies";
 import TechItem from "../techItem/techItem";
 
 const TechStack: React.FC = () => {
-  return (
-    <div
-      className="flex flex-wrap gap-16 max-w-6xl mx-auto items-center justify-center "
-      data-testid="techItemGrid"
-    >
-      {technologies.map((el) => (
-        <TechItem logo={el.logo} name={el.name} key={el.logo} />
-      ))}
-    </div>
-  );
+    return (
+        <div
+            className="flex flex-wrap gap-8 mt-8 max-w-6xl mx-auto items-center justify-center "
+            data-testid="techItemGrid"
+        >
+            {technologies.map((el) => (
+                <TechItem logo={el.logo} name={el.name} key={el.logo} />
+            ))}
+        </div>
+    );
 };
 
 export default TechStack;
