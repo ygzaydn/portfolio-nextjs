73b17ccd2b126c046f9a681a50be999a774a6194
ygzaydn <ygzaydns@gmail.com>
2023-08-14 16:10:43 +0300

    upd

--- components/contactGrid/contactGrid.tsx
+++ components/contactGrid/contactGrid.tsx
@@ -13,9 +13,21 @@ const ContactGrid: React.FC = () => {
                 visible: { opacity: 1, scale: 1 },
                 hidden: { opacity: 0, scale: 1 },
             }}
-            className="group/stack flex flex-col max-w-7xl mx-auto px-16 my-16 bg-slate-100 py-6"
+            className="group/stack relative flex flex-col max-w-7xl mx-auto px-16 my-36 bg-slate-100 py-6 rounded-xl ring-2 ring-blue-800 border-2 sm:overflow-x-hidden"
             id="contactGrid"
         >
+            <motion.div
+                initial="hidden"
+                whileInView="visible"
+                viewport={{ once: true }}
+                transition={{ duration: 1, delay: 1.5 }}
+                variants={{
+                    visible: { opacity: 1, scale: 1 },
+                    hidden: { opacity: 0, scale: 1 },
+                }}
+                className="absolute max-w-7xl w-full h-full bg-blue-800 -z-10 rounded-xl inset-0 !rotate-3 !scale-105"
+                id="contactGrid"
+            />
             <Title title="Contact Form" color="blue" />
             <div className="max-w-80">
                 <Form />
